a11091249fd27340bf70d9d366df3d204a3eba82
Matthew Middlebrook <mkm15a@acu.edu>
2019-12-01 15:04:55 -0600

    Finished 3D Modeling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(
@@ -20,4 +22,40 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   }
+
+    // Dropdowns
+    var $dropdowns = getAll('.dropdown:not(.is-hoverable)');
+
+    if ($dropdowns.length > 0) {
+      $dropdowns.forEach(function ($el) {
+        $el.addEventListener('click', function (event) {
+          event.stopPropagation();
+          $el.classList.toggle('is-active');
+        });
+      });
+  
+      document.addEventListener('click', function (event) {
+        closeDropdowns();
+      });
+    }
+  
+    function closeDropdowns() {
+      $dropdowns.forEach(function ($el) {
+        $el.classList.remove('is-active');
+      });
+    }
+  
+    // Close dropdowns if ESC pressed
+    document.addEventListener('keydown', function (event) {
+      var e = event || window.event;
+      if (e.keyCode === 27) {
+        closeDropdowns();
+      }
+    });
+  
+    // Functions
+  
+    function getAll(selector) {
+      return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+    }
 });
